ca02bf745f452c423087cf38db148ea932a25d03
Saren Currie <saren@sarencurrie.com>
2015-09-24 11:37:35 +1200

    Added player sprite and movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public float speed = 6.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+	}
+}
