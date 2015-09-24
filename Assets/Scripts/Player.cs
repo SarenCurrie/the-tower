3b0832cc329a963949831ca946231c151a16c186
Saren Currie <saren@sarencurrie.com>
2015-09-24 12:41:03 +1200

    Player rotates to face cursor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -28,5 +28,10 @@ public class Player : MonoBehaviour {
 		{
 			transform.position += Vector3.down * speed * Time.deltaTime;
 		}
+		Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+		Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+		lookPos = lookPos - transform.position;
+		float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 	}
 }
