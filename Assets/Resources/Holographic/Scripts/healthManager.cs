b2da1af0f96041c13677188af333a7f0e6f8bedc
Jacob Holden <yacoob.holden@gmail.com>
2015-09-26 11:07:11 +1200

    Get scrolling health done

---
+++ Assets/Resources/Holographic/Scripts/healthManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class healthManager : MonoBehaviour
+{
+	//An integer to advance frames
+	private int frameCounter = 0;	
+	private float delay = 0.04f;
+	private int t=85;
+	private Object[] objects;
+	private Sprite[] sprites;
+	private Image mySprite;
+	private float health = 100f;
+	
+	void Awake(){
+		// store all sprites
+		this.mySprite = this.GetComponent<Image>();
+	}
+
+	void Start ()
+	{
+		//Load all textures found on the Sequence folder, that is placed inside the resources folder
+		this.objects = Resources.LoadAll("Holographic/output/pulse", typeof(Sprite));
+		
+		//Initialize the array of sprites with the same size as the objects array
+		this.sprites = new Sprite[objects.Length];
+		
+		//Cast each Object to Sprite and store the result inside the Sprites array
+		for(int i=0; i < objects.Length;i++)
+		{
+			this.sprites[i] = (Sprite)this.objects[i];
+		}
+
+	}
+	
+	void Update ()
+	{t++;
+		if (t>10){
+			t=0;
+		}
+		StartCoroutine("PlayLoop",delay);
+		mySprite.sprite = sprites[frameCounter];
+	}
+	
+	//The following methods return a IEnumerator so they can be yielded:
+	//A method to play the animation in a loop
+	IEnumerator PlayLoop(float delay)
+	{
+		//Wait for the time defined at the delay parameter
+		yield return new WaitForSeconds(delay);  
+		
+		//Advance one frame
+		frameCounter = (++frameCounter)%sprites.Length;
+		
+		//Stop this coroutine
+		StopCoroutine("PlayLoop");
+	}  
+	
+	//A method to play the animation just once
+	IEnumerator Play(float delay)
+	{
+		//Wait for the time defined at the delay parameter
+		yield return new WaitForSeconds(delay);  
+		
+		//If the frame counter isn't at the last frame
+		if(frameCounter < sprites.Length)
+		{
+			//Advance one frame
+			++frameCounter;
+		}
+		
+		//Stop this coroutine
+		StopCoroutine("PlayLoop");
+	} 
+	
+}
