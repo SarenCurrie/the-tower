d30aa7a138ca92f881f19c10d7390f117a4e239f
Harry She <hshe824@aucklanduni.ac.nz>
2015-09-26 00:01:03 +1200

    high score updating

--- Assets/Resources/Holographic/Scripts/UIManagerScript.cs
+++ Assets/Resources/Holographic/Scripts/UIManagerScript.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class UIManagerScript : MonoBehaviour {
 
+    private int playerScore = 0;
 	// Use this for initialization
 	void Start () {
-	
+        this.transform.Find("Score").GetComponent<Text>().text = playerScore.ToString();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        playerScore++;
+        this.transform.Find("Score").GetComponent<Text>().text = playerScore.ToString();
+
+       
 	
 	}
 
