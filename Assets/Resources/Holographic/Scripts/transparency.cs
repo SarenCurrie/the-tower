252a05ce5e799debc38b365d440bbd7007be4d04
Jacob Holden <yacoob.holden@gmail.com>
2015-09-26 11:56:43 +1200

    Make transparency script work

--- Assets/Resources/Holographic/Scripts/transparency.cs
+++ Assets/Resources/Holographic/Scripts/transparency.cs
@@ -4,11 +4,16 @@ using System.Collections;
 
 public class transparency : MonoBehaviour
 {
-	private float a=1f; //alpha control
+	private float a=0.01f; //alpha control
 	void Awake()
 	{
-		a++;
-		this.GetComponent<Image>().color = new Color(this.GetComponent<Image>().color.r,this.GetComponent<Image>().color.b,this.GetComponent<Image>().color.g,.65f*a);
+		a = a + 0.01f;
+		if (this.GetComponent<Image> ()) {
+			this.GetComponent<Image> ().color = new Color (this.GetComponent<Image> ().color.r, this.GetComponent<Image> ().color.b, this.GetComponent<Image> ().color.g, .65f * a);
+	
+		} else if (this.GetComponent<Text> ()) {
+			this.GetComponent<Text> ().color = new Color (this.GetComponent<Text> ().color.r, this.GetComponent<Text> ().color.b, this.GetComponent<Text> ().color.g, .65f * a);
+		}
 	}
 
 	void Start ()
@@ -19,11 +24,13 @@ public class transparency : MonoBehaviour
 	void Update ()
 	{
 	
-		if (a!=sliders.opacity){
-			a= sliders.opacity;
-			this.GetComponent<Image>().color = new Color(this.GetComponent<Image>().color.r,this.GetComponent<Image>().color.b,this.GetComponent<Image>().color.g,.65f*a);
+		a=a + 0.01f;
+		if (this.GetComponent<Image> ()) {
+			this.GetComponent<Image> ().color = new Color (this.GetComponent<Image> ().color.r, this.GetComponent<Image> ().color.b, this.GetComponent<Image> ().color.g, .65f * a);
+			
+		} else if (this.GetComponent<Text> ()) {
+			this.GetComponent<Text> ().color = new Color (this.GetComponent<Text> ().color.r, this.GetComponent<Text> ().color.b, this.GetComponent<Text> ().color.g, .65f * a);
 		}
-		
 	}
 
   
