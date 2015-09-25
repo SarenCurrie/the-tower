e8b9ec84910ea7c67fc203db14833b395d80130d
Saren Currie <saren@sarencurrie.com>
2015-09-25 16:43:41 +1200

    Projectiles now die

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -15,9 +15,9 @@ public class Projectile : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        GameObject.Destroy(GetComponent<GameObject>(), killTime);
+        Destroy(gameObject, killTime);
     }
-	
+
 	// Update is called once per frame
 	void Update () {
         CheckForCollision();
