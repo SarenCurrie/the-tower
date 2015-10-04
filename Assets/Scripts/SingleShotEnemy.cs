780ab678abce7cffeacb614581e6282b6a26c778
Hamish Brebner <hamishbrebner@hotmail.com>
2015-10-04 16:53:39 +1300

    Added scripts and prefabs for two enemy types.

---
+++ Assets/Scripts/SingleShotEnemy.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SingleShotEnemy : MonoBehaviour
+{
+
+	private const float BASE_HIT_DAMAGE = 0.01f;
+
+	public Transform projectilePrefab;
+
+	private int spread = 1;
+	private float spreadRange;
+	private float fireForce;
+	private float fireFrequency;
+	private float damageMod;
+
+	private float lastFired = 0;
+
+	public float minBurstTime;
+	public float maxBurstTime;
+
+	public float minFireWait;
+	public float maxFireWait;
+
+	public float damage;
+
+	private float nextFireTime = 0;
+	private float fireStopTime = 0;
+	private bool waitingToFire = false;
+
+	void Start()
+	{
+		Generate();
+	}
+
+	void Update()
+	{
+		MaybeFireAtPlayer();
+	}
+
+	public void Fire()
+	{
+		Fire(CalculateDamage());
+	}
+
+	public void Fire(float damage)
+	{
+		if (Time.time > lastFired + 1 / fireFrequency)
+		{
+			for (int i = 0; i < spread; i++)
+			{
+				Transform projectile = Instantiate(projectilePrefab, transform.position, transform.rotation) as Transform;
+				Transform projectileTransform = projectile.GetComponent<Transform>();
+				if (spread > 1)
+				{
+					projectileTransform.Rotate(new Vector3(0, 0, -(spreadRange / 2) + i * (spreadRange / (spread - 1))));
+				}
+				projectile.GetComponent<Rigidbody2D>().AddForce((projectileTransform.up) * fireForce);
+				projectile.GetComponent<Projectile>().SetDamage(damage);
+			}
+			lastFired = Time.time;
+		}
+	}
+
+	private float CalculateDamage()
+	{
+		return (1.0f * damageMod);
+	}
+
+	public void Generate()
+	{
+		spread = 1;
+		fireForce = UnityEngine.Random.Range(20, 35);
+		fireFrequency = UnityEngine.Random.Range(15f, 30);
+		spreadRange = 1;
+		damageMod = (float)((50 * 25) / 
+			(20 * (System.Math.Pow(fireForce, 0.2)) + 15 * (System.Math.Pow(fireFrequency, 1.2))));
+	}
+
+	private void MaybeFireAtPlayer()
+	{
+		if (!waitingToFire)
+		{
+			if (Time.time > fireStopTime)
+			{
+				nextFireTime = Time.time + Random.Range(minFireWait, maxFireWait);
+				waitingToFire = true;
+			}
+			else
+			{
+				Fire();
+			}
+		}
+		else if (Time.time > nextFireTime)
+		{
+			fireStopTime = Time.time + Random.Range(minBurstTime, maxBurstTime);
+			waitingToFire = false;
+		}
+	}
+}
