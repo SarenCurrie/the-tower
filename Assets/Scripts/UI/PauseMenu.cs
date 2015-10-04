8566c06b17c80fd722e75148f2482c996036973b
Jacob Holden <yacoob.holden@gmail.com>
2015-10-04 14:50:51 +1300

    Add start screen and pause screen

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+
+	public GUISkin mySkin;
+	private static float pauseHeight = 300f;
+	private static float pauseWidth = 280f;
+	private static float pauseTop = 0 - Screen.height;
+	private static float pauseBottom = (Screen.height - pauseHeight)/2;
+	private Rect pauseRect = new Rect((Screen.width - pauseWidth)/2, pauseTop, pauseWidth, pauseHeight);
+	public static bool paused = false;
+
+	void PauseArea(int windowID) 
+	{
+		GUILayout.BeginHorizontal();
+		GUILayout.Space(10);
+		GUILayout.BeginVertical();
+		
+		GUILayout.BeginHorizontal ();
+		GUILayout.FlexibleSpace();
+		GUILayout.Label("PAUSED", "ShortLabel");
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal ();
+
+		GUILayout.BeginHorizontal ();
+		GUILayout.Label("", "Divider");
+		GUILayout.EndHorizontal ();
+		
+		GUILayout.BeginHorizontal ();
+		GUILayout.FlexibleSpace();
+		if (GUILayout.Button("Resume", "ShortButton")) {
+			// Resume the time
+			paused = false;
+		}
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal ();
+		
+		GUILayout.BeginHorizontal ();
+		GUILayout.FlexibleSpace();
+		GUI.enabled = false;
+		GUILayout.Button("Video Options", "ShortButton");
+		GUI.enabled = true;
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal ();
+
+		GUILayout.BeginHorizontal ();
+		GUILayout.FlexibleSpace();
+		GUI.enabled = false;
+		GUILayout.Button("Game Options", "ShortButton");
+		GUI.enabled = true;
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal ();
+		
+		GUILayout.BeginHorizontal ();
+		GUILayout.FlexibleSpace();
+		if (GUILayout.Button("Quit", "ShortButton")) {
+			// Exit the game
+			Application.Quit();
+		}
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal ();
+		
+		GUILayout.EndVertical();
+		GUILayout.Space(10);
+		GUILayout.EndHorizontal();
+	}
+	
+	void Start () {
+	}
+	
+	void Update() {;
+		// On ESCAPE, pause the game
+		if (Input.GetKey(KeyCode.Escape)) {
+			paused = true;
+		}
+		if (paused) {
+			// Pause time
+			Time.timeScale = 0.0f;
+		} else {
+			// Unpause the game.
+			Time.timeScale = 1.0f;
+		}
+	}
+
+	void OnGUI () {
+		GUI.skin = mySkin;
+		// Create pause window
+		pauseRect = GUI.Window (5, pauseRect, PauseArea, "");
+		if (paused) {
+			// Drop box from top
+			pauseRect.y = Mathf.MoveTowards (pauseRect.y, pauseBottom, 10);
+		} else {
+			// Move back to top
+			pauseRect.y = Mathf.MoveTowards (pauseRect.y, pauseTop, 10);
+		}
+	}
+}
---
+++ Assets/Scripts/UI/WelcomeScreenImage.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WelcomeScreenImage : MonoBehaviour {
+		
+	public GUISkin mySkin;
+	public string firstLevelName;
+	public static bool continueEnabled = false;
+	// Start rect above the screen
+	private Rect buttonRect = new Rect(Screen.width/20, 0 - Screen.height, 280, 230);
+	private bool onStartScreen = true;
+
+	void ButtonArea(int windowID){
+		GUILayout.BeginHorizontal();
+		GUILayout.Space(10);
+		GUILayout.BeginVertical();
+		
+		GUILayout.BeginHorizontal ();
+		GUILayout.FlexibleSpace();
+		GUILayout.Label("THE TOWER", "ShortLabel");
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal ();
+
+		GUILayout.BeginHorizontal ();
+		GUILayout.FlexibleSpace();
+		if (GUILayout.Button("Begin", "ShortButton")) {
+			// Start game here
+			Application.LoadLevel(firstLevelName);
+		}
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal ();
+
+		GUILayout.BeginHorizontal ();
+		GUILayout.FlexibleSpace();
+		GUI.enabled = continueEnabled;
+		GUILayout.Button("Continue", "ShortButton");
+		GUI.enabled = true;
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal ();
+
+		GUILayout.BeginHorizontal ();
+		GUILayout.FlexibleSpace();
+		if (GUILayout.Button("Quit", "ShortButton")) {
+			// Exit the game
+			Application.Quit();
+		}
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal ();
+
+		GUILayout.EndVertical();
+		GUILayout.Space(10);
+		GUILayout.EndHorizontal();
+	}
+
+
+	// Use this for initialization
+	void Start() {
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	}
+
+	void OnGUI() {
+		GUI.skin = mySkin;
+		buttonRect = GUI.Window (1, buttonRect, ButtonArea, "");
+		// Drop start box from top
+		buttonRect.y = Mathf.MoveTowards(buttonRect.y,Screen.height/16,5);
+	}
+
+}
