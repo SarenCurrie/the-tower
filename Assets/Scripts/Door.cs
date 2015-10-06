8fdc7af7b1bddd14f03039bd52a67a5dfffa0873
Max Lay <kalda341@gmail.com>
2015-10-06 19:57:06 +1300

    Delay before door teleports player

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,15 +6,36 @@ public class Door : MonoBehaviour {
     public enum DOOR_ORIENTATION { TOP, BOTTOM, LEFT, RIGHT, DISABLED };
     public DOOR_ORIENTATION orientation;
 
-    public const float DOOR_MOVEMENT_VALUE = 1.5f;
+	//How far to move player
+    public const float DOOR_MOVEMENT_VALUE = 1.3f;
 
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.transform.tag == Tags.PLAYER && orientation != DOOR_ORIENTATION.DISABLED)
-        {
+	public const float DOOR_CONTACT_TIME = 1;
+
+	private float collision_start_time = Mathf.Infinity;
+
+	public void Update()
+	{
+		if (Time.time > collision_start_time + DOOR_CONTACT_TIME)
+		{
 			if (GameManager.currentFloor.currentRoom.EnemiesLeft() == 0)
 			{
 				GameManager.currentFloor.MovePlayerRoom(this);
 			}
-        }
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.transform.tag == Tags.PLAYER && orientation != DOOR_ORIENTATION.DISABLED)
+		{
+			collision_start_time = Mathf.Infinity;
+		}
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.transform.tag == Tags.PLAYER && orientation != DOOR_ORIENTATION.DISABLED)
+		{
+			collision_start_time = Time.time;
+		}
     }
 }
