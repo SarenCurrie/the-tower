e1ef62736bce97a9fe0519b767a3942c13b426f7
Hamish Brebner <hamishbrebner@hotmail.com>
2015-10-08 20:02:41 +1300

    temp commit to get latest changes. I'm sorry scur

---
+++ Assets/Scripts/ExplosionEnemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionEnemy : MonoBehaviour {
+
+	public float explosionForce;
+	public float explosionRadius;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MaybeExplode();
+	}
+
+	void MaybeExplode()
+	{
+		
+		Rigidbody2D body = GetComponent<Rigidbody2D>();
+		GameObject player = GameObject.FindWithTag("Player");
+		if (body.IsTouching(player.GetComponent<CircleCollider2D>()))
+		{
+			Vector3 explosionPosition = body.transform.position;
+			foreach (Enemy enemy in GameManager.currentFloor.currentRoom.GetEnemiesInRoom())
+			{
+				enemy.GetComponent<RigidbodyExplansion>().Explosion(enemy.GetComponent<RigidBody>(),
+					explosionForce, explosionPosition, explosionRadius);
+			}
+			player.GetComponent<RigidbodyExpansion>().Explosion(player.GetComponent<RigidBody2D>(),
+			   explosionForce, explosionPosition, explosionRadius);
+		}
+    }
+}
---
+++ Assets/Scripts/RigidbodyExpansion.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigidBodyExpansion : MonoBehaviour {
+
+	public static void AddExplosionForce(Rigidbody2D body, float explosionForce, Vector3 explosionPosition, float explosionRadius)
+	{
+		var dir = (body.transform.position - explosionPosition);
+		float wearoff = 1 - (dir.magnitude / explosionRadius);
+		body.AddForce(dir.normalized * explosionForce * wearoff);
+	}
+
+}
