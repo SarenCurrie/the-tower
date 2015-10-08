c8d83058e6336dcce8b397482fbad1e738eb5576
Max Lay <kalda341@gmail.com>
2015-10-08 23:37:28 +1300

    Floors implemented properly

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,23 +3,42 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	public GameObject floorPrefab;
+	//Non static fields can be edited with the Unity editor.
+	public GameObject[] floorPrefabs;
+	//Will be set to floorPrefabs
+	public static GameObject[] staticFloorPrefabs;
 
-    public GameObject playerPrefab;
+	//The player to instantiate
+	public GameObject playerPrefab;
 
     public static GameObject player;
 
-    public static Floor currentFloor;
+	//Player does not start on a floor
+	private static int currentFloorNumber = -1;
+	public static Floor currentFloor;
     
     void Start () {
-		currentFloor = Instantiate(floorPrefab).GetComponent<Floor>();
-		currentFloor.GenerateFloor();
+		staticFloorPrefabs = floorPrefabs;
+
 		player = Instantiate(playerPrefab) as GameObject;
-		currentFloor.MovePlayerToFloor(player);
+		MovePlayerToNextFloor();
     }
 
     public static GameObject GetPlayer()
     {
 		return player;
     }
+
+	public static void MovePlayerToNextFloor()
+	{
+		if (currentFloor != null)
+			Destroy(currentFloor);
+
+		currentFloorNumber++;
+
+		//Spawn the floor, generate it, and move the player to it
+		currentFloor = Instantiate(staticFloorPrefabs[currentFloorNumber]).GetComponent<Floor>();
+		currentFloor.GenerateFloor();
+		currentFloor.MovePlayerToFloor(player);
+	}
 }
