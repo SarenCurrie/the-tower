3d2035e13def06cdcdda71d049c2d8a27dd19d65
Tate <borigthedwarf@gmail.com>
2015-10-09 15:19:23 +1300

    Fixed null projectile bug.

--- Assets/Scripts/UnitHealth.cs
+++ Assets/Scripts/UnitHealth.cs
@@ -72,6 +72,10 @@ public class UnitHealth : MonoBehaviour {
      */
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.GetComponent<Projectile>() == null)
+            return;
+
+
         int ind = Array.IndexOf(damagedBy, collision.gameObject.tag);
         if (ind > -1)
         {
