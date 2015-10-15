8de6844cc8d79fd915287b85a8c951c2711ada1f
kokobrainz <logan.horton@live.com>
2015-10-15 13:53:30 +1300

    started the boss setup

---
+++ Assets/Scripts/Enemy/BossBehaviour_2.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossBehaviour_2 : Enemy {
+    private float LastChecked = 0;
+    public float MaxHealth;
+
+    private float attackTick = 0;
+    public float attackTime;
+    
+    public int baseScore;
+
+    private Rigidbody2D rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetPlayer() == null)
+            return;
+
+    }
+
+    private Player GetPlayer()
+    {
+        return GameObject.FindObjectOfType<Player>();
+    }
+
+    private Vector3 GetRelativePlayerPosition()
+    {
+        return GetPlayer().GetComponent<Transform>().position - transform.position;
+    }
+
+    private void RotateToFacePlayer()
+    {
+        Vector3 relativePlayerPos = GetRelativePlayerPosition();
+        float angle = Mathf.Atan2(relativePlayerPos.y, relativePlayerPos.x) * Mathf.Rad2Deg + 270;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+    
+}
