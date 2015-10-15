0185a1365b81523f8db2f666f7f4106a460ae9d3
oratorio <frankie.lam54@hotmail.com>
2015-10-15 17:47:42 +1300

    Fixed explosions on first boss and first boss spawning gun

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -13,7 +13,7 @@ public class Explosion : MonoBehaviour {
 	public static void CreateExplosion(GameObject explosionPrefab, float explosionForce, float explosionRadius, float damage, Vector3 position)
 	{
 		GameObject explosion = Instantiate(explosionPrefab, position, Quaternion.identity) as GameObject;
-		explosion.GetComponent<Explosion>().SetupExplosion(explosionForce, explosionRadius, damage);
+        explosion.GetComponent<Explosion>().SetupExplosion(explosionForce, explosionRadius, damage);
 	}
 
 	private void SetupExplosion(float explosionForce, float explosionRadius, float damage)
@@ -28,19 +28,19 @@ public class Explosion : MonoBehaviour {
 		//Wait until fields are set
 		if (explosionForce == -1 || explosionRadius == -1 || damage == -1)
 			return;
-
 		//The position of this suicide enemy
 		Vector3 explosionPosition = GetComponent<Rigidbody2D>().transform.position;
 
-		//Loop through all enemies in the room and damage + move them
-		foreach (GameObject enemy in GameManager.currentFloor.currentRoom.GetEnemiesInRoom())
+        //Loop through all enemies in the room and damage + move them
+        foreach (GameObject enemy in GameManager.currentFloor.currentRoom.GetEnemiesInRoom())
 		{
-			enemy.GetComponent<RigidBodyExpansion>().Explosion(explosionForce, explosionPosition, explosionRadius, damage);
+            if (enemy.GetComponent<RigidBodyExpansion>() != null)
+                enemy.GetComponent<RigidBodyExpansion>().Explosion(explosionForce, explosionPosition, explosionRadius, damage);
+            else
+                print("Error: There is a RigidBodyExpansion missing from an enemy");
 		}
-
-		//Damage + Move the player
-		GameManager.GetPlayer().GetComponent<RigidBodyExpansion>().Explosion(explosionForce, explosionPosition, explosionRadius, damage);
-
-		Destroy(this);
+        //Damage + Move the player
+        GameManager.GetPlayer().GetComponent<RigidBodyExpansion>().Explosion(explosionForce, explosionPosition, explosionRadius, damage);
+        Destroy(this.gameObject, 0.5f);
 	}
 }
---
+++ Assets/Scripts/SpawningShot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawningShot : Projectile
+{
+    public GameObject rotator;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, killTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(rotator, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
