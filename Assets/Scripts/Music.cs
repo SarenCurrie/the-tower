6c319a6e8ac948e1d6ec2cf9ef78d099846f72ba
Saren Currie <saren@sarencurrie.com>
2015-10-17 19:29:47 +1300

    Fixed music to work with custom weapon

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -3,8 +3,23 @@ using System.Collections;
 
 public class Music : MonoBehaviour
 {
+	private static Music instance = null;
+	public static Music Instance {
+		get { return instance; }
+	}
+
 	public AudioClip mainMusic;
 	public AudioClip bossMusic;
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
 
 	public void StartMainMusic()
 	{
