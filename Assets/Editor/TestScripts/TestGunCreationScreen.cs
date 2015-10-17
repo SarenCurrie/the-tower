e5f72ae80f05aca6ab096283bcf1dc321da53f73
hshe824 <hshe824@aucklanduni.ac.nz>
2015-10-18 11:36:20 +1300

    Added testing creation screen

---
+++ Assets/Editor/TestScripts/TestGunCreationScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+using NSubstitute;
+
+[TestFixture]
+public class TestGunCreationScreen
+{
+    ScriptInstantiator instantiator = new ScriptInstantiator();
+
+    [Test]
+    public void TestCreateGunCreationScreen()
+    {
+
+        GunCreationScreen gunCreationScreen = instantiator.InstantiateScript<GunCreationScreen>();
+        Assert.IsNotNull(gunCreationScreen);
+    }
+
+}
--- Assets/Editor/TestScripts/TestUIController.cs
+++ Assets/Editor/TestScripts/TestUIController.cs
@@ -17,4 +17,24 @@ public class TestUIController
 
     }
 
+    [Test]
+    public void TestUIFixedResolution()
+    {
+        UIController uiController = instantiator.InstantiateScript<UIController>();
+        Assert.AreEqual(768f,UIController.targetHeight);
+        Assert.AreEqual(1024f,UIController.targetWidth);
+
+    }
+
+     [Test]
+    public void TestUIGUIMatrix()
+    {
+        UIController uiController = instantiator.InstantiateScript<UIController>();
+        Matrix4x4 matrix = UIController.GetGUIMatrix();
+        Assert.IsNotNull(matrix);
+
+    }
+
+
+
 }
